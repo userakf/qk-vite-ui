fdf8fec863533b2e87d586a10db9758f406055d2
userakf <820279897@qq.com>
2022-08-19 23:37:53 +0800

    style:删除无用代码

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,6 @@
 import { createApp } from 'vue'
 import SmartyUI from './entry'
 
-import SButton from './button/Button'
-import JSXButton from './JSXButton'
-import SFCButton from './SFCButton.vue'
-
 function bootstrap() {
     const app = createApp({
         template:`
