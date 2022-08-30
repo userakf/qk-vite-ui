36886b4d8f52bb4b1ba9356de44aafdb29df9b2c
userakf <820279897@qq.com>
2022-08-30 23:36:08 +0800

    feat:ts

---
+++ src/button/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div :ref="colRefs" v-for="item in arr">
+        {{ item }}
+    </div>
+</template>
+
+<script setup lang="ts">
+import { reactive } from 'vue'
+const resList = reactive([])
+const arr = reactive([])
+const colRefs = (ref: Element, refs: Record<string, any> = {}) => {
+    resList.push(ref)
+}
+</script>
+<style lang="scss" scoped>
+</style>
